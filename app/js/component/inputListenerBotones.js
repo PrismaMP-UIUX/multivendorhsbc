7999e3a3b1ba105b3d1ec2cf31f6efa8e04e0cba
RODRIGO PERROTE <rodrigoperrote@gmail.com>
2017-07-28 15:32:54 -0300

    Se corrije el error cuando no se usa el listener de botones

--- app/js/component/inputListenerBotones.js
+++ app/js/component/inputListenerBotones.js
@@ -1,22 +1,35 @@
 function alescribir(campo,boton){
 	var campo = document.getElementById(campo);
-	campo.focus();
-	var boton = document.getElementById(boton)
-	boton.style.display="none";
-	campo.addEventListener("input", function (e) {
-	if(this.value.length>0){
-		boton.style.display="block";
-	}else{
-		boton.style.display="none";
+	if(campo!=null)
+	{
+		campo.focus();
+		var boton = document.getElementById(boton)
+		if(boton!=null)
+		{
+			boton.style.display="none";
+			campo.addEventListener("input", function (e) {
+				if(this.value.length>0){
+					boton.style.display="block";
+				}else{
+					boton.style.display="none";
+				}
+			});
+		}
 	}
-}); 
-}
+} 
+
 function limpiarPass(campo){
 	var campo = document.getElementById(campo);
-	campo.focus();
-	campo.value="";
-	var borrar = document.getElementById("borrar");
-	var continuar = document.getElementById("continuar");
-	borrar.style.display="none";
-	continuar.style.display="none";
+	if(campo!=null){
+		campo.focus();
+		campo.value="";
+		var borrar = document.getElementById("borrar");
+		if(borrar!=null){
+			var continuar = document.getElementById("continuar");
+			if(continuar!=null){
+				borrar.style.display="none";
+				continuar.style.display="none";
+			}
+		}
+	}
 }
