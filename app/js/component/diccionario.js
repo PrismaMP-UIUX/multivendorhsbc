8a54db2cd4d84d211b0384b943451ed7eeded719
Patricio Graglia <pgraglia@intranet.banelco.com.ar>
2017-09-20 14:42:04 -0300

    Actualización

---
+++ app/js/component/diccionario.js
@@ -0,0 +1,29 @@
+var siglas = [
+	'hsbc','Hsbc', 
+	'icbc', 'Icbc',
+	'afip', 'Afip',
+	'agip', 'Agip',
+	'anses', 'Anses',
+	'pmc', 'Pmc',
+	'sms', 'SMS',
+	'bst', 'Bst',
+	'smsv', 'Smsv'
+];
+var nombresPropios = [
+	'todo pago', 'Todo pago', 
+	'pago mis cuentas', 'Pago mis cuentas',
+	'banelcomovil', 'Banelcomovil', 
+	'macro', 'Macro',
+	'frances', 'Frances',
+	'galicia', 'Galicia',
+	'comafi', 'Comafi',
+	'santander rio', 'Santander rio', 
+];
+var txtDestacados = [
+	'home banking', 'Home banking',
+	'at.', 'At.',
+	'ciudad', 'Ciudad',
+	'banca telefonica', 'Banca telefonica',
+	'banca movil', 'banca movil', 
+	'terminal autoservicio', 'Terminal autoservicio',
+]
